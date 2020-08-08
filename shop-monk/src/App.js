e27f0c21f3d0023de39f90da4f28938f020b725b
shubham-shri <shubham.shri1703@gmail.com>
2020-08-08 20:31:15 +0530

    Added Home Page

---
+++ shop-monk/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './HomePage'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
+
