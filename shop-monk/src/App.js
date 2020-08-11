086da923383eb39561ee2701a6ef2f9551e34616
shubham-shri <shubham.shri1703@gmail.com>
2020-08-11 19:36:00 +0530

    updating app with a shop page, colleciton preview component and collection item component

--- shop-monk/src/App.js
+++ shop-monk/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
         <HomePage />
       </Switch>
     </div>
