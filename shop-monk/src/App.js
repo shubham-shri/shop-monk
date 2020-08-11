69a1456e9810399ba1f1066bb862aa8635b4dea2
shubham-shri <shubham.shri1703@gmail.com>
2020-08-11 19:56:22 +0530

    added header component and links to home, shop page and contact

--- shop-monk/src/App.js
+++ shop-monk/src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+
 import './App.css'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
