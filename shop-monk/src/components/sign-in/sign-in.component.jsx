42794a5f6a655c85acb88bfd7dfacd97dc065648
shubham-shri <shubham.shri1703@gmail.com>
2020-08-14 20:35:25 +0530

    finished sign in handle submit method

--- shop-monk/src/components/sign-in/sign-in.component.jsx
+++ shop-monk/src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import CustomButton from '../custom-button/custom-button.component'
 import FormInput from '../form-input/form-input.component'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 import './sign-in.styles.scss'
 
 export default class SignIn extends Component {
@@ -12,9 +12,16 @@ export default class SignIn extends Component {
       password: '',
     }
   }
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault()
-    this.setState({ email: '', password: '' })
+    const { email, password } = this.state
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({ email: '', password: '' })
+    } catch (err) {
+      console.log(err)
+    }
   }
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value })
