80432b37fd1b57f3cb5f08dafc75fa64083d1c83
shubham-shri <shubham.shri1703@gmail.com>
2020-08-15 12:29:48 +0530

    added cartItem and cartDropdown components and implemented cart reducer

--- shop-monk/src/components/header/header.component.jsx
+++ shop-monk/src/components/header/header.component.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -26,11 +28,14 @@ const Header = ({ currentUser }) => (
           Sign In{' '}
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 )
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 })
 
 export default connect(mapStateToProps)(Header)
---
+++ shop-monk/src/redux/cart/cart.type.js
@@ -0,0 +1,4 @@
+const cartActionTypes = {
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+}
+export default cartActionTypes
