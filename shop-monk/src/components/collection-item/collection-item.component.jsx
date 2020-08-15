5d1cdc277314b124ab5251e3129c5f0d6f2e1d28
shubham-shri <shubham.shri1703@gmail.com>
2020-08-15 14:23:38 +0530

    updated shop data with new data, updated cart utils to group items in cart reducer

--- shop-monk/src/components/collection-item/collection-item.component.jsx
+++ shop-monk/src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import CustomButton from '../custom-button/custom-button.component'
+import { connect } from 'react-redux'
+import { addItem } from '../../redux/cart/cart.action'
 import './collection-item.styles.scss'
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,7 +13,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add To Cart
+      </CustomButton>
     </div>
   )
 }
-export default CollectionItem
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+})
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- shop-monk/src/redux/cart/cart.type.js
+++ shop-monk/src/redux/cart/cart.type.js
@@ -1,4 +1,5 @@
 const cartActionTypes = {
   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM',
 }
 export default cartActionTypes
