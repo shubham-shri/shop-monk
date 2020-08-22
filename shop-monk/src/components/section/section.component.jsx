f4cabebc718e5544129631855bc13314749ebd3b
shubham-shri <shubham.shri1703@gmail.com>
2020-08-22 22:31:24 +0530

    added styles to the section component

--- shop-monk/src/components/section/section.component.jsx
+++ shop-monk/src/components/section/section.component.jsx
@@ -3,21 +3,36 @@ import './section.styles.scss'
 
 export default class SectionPage extends Component {
   render() {
-    const { coverImage } = this.props.sectionData
+    const { coverImage, subSections } = this.props.sectionData
+    console.log(this.props)
     return (
       <div className="section-container">
-        <div
-          className="section-container__banner"
-          style={{ backgroundImage: `url(${coverImage})` }}
-        />
+        <div className="section-container__banner">
+          <img className="banner-image" src={coverImage} alt="banner" />
+        </div>
         <div className="section-container__heading">Browse</div>
         <div className="section-container__sub-section-container">
-          <div className="section-container__sub-section-container__sub-section">
-            top wear
-          </div>
-          <div className="section-container__sub-section-container__sub-section">
-            bottom wear
-          </div>
+          {subSections.map((section) => (
+            <div className="section-container__sub-section-container__sub-section">
+              {section.title}
+              <div className="section-container__sub-section-container__sub-section__item-container">
+                {section.categorySection.map((category) => (
+                  <div className="section-container__sub-section-container__sub-section__item-container__item">
+                    <div>
+                      <img
+                        className="category-image"
+                        src={`${category.categoryImage}`}
+                        alt="category-item"
+                      />
+                    </div>
+                    <div className="sub-section__category-items">
+                      {category.categoryName}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     )
